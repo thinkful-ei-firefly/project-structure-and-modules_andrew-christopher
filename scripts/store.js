015e6a78fc04da1e438f29ca16daaf82d9323d6b
cmpowers1981 <c.m.powers@outlook.com>
2019-07-17 13:43:43 -0400

    Chris Commit

--- scripts/store.js
+++ scripts/store.js
@@ -12,5 +12,58 @@ const STORE = (function () {
   ];
   let hideCompleted = false;
   let searchTerm = null;
-  return {items, hideCompleted, searchTerm};
-}() );
+  
+  function findById() {
+    return items.find((item) => id === item.id); 
+  }
+
+  function addItem(name) {
+    try {
+      item.validateName(name);
+      this.items.push(item.create(name));
+    }
+    catch(error) {
+      console.log(`Cannot add item: ${error.message}`)
+    }
+    
+    function findAndToggleChecked(id) {
+      this.findById(id).checked = !this.findById(id).checked;
+    }
+  }
+
+    function findAndUpdateName(id, newName) {
+      try {
+        item.validateName(newName);
+        this.findById(id).name = newName;
+      }
+      catch(error) {
+        console.log(`Cannot update name: ${error.message}`);
+      }
+
+    function findAndDelete(id) {
+      this.items = this.items.filter(item => item.id !== id);
+    }
+
+    function toggleCheckedFilter() {
+      this.hideCheckedItems = !this.hideCheckedItems;
+    }
+
+    function setSearchTerm(val) {
+      STORE.searchTerm = val;
+    }
+
+  }
+
+  return {items,
+      hideCompleted,
+      searchTerm,
+      findById,
+      addItem,
+      findAndToggleChecked,
+      findAndUpdateName,
+      findAndDelete,
+      toggleCheckedFilter,
+      setSearchTerm
+  };
+}());
+
